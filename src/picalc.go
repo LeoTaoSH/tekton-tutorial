5444801482e8c5072a0410e50273ab40c4b30c76
leotao <taoxb@163.com>
2019-11-23 15:20:09 +0800

    1123

--- src/picalc.go
+++ src/picalc.go
@@ -14,7 +14,7 @@ func say(word string) string {
 
 func handler(w http.ResponseWriter, r *http.Request) {
     log.Print("Say something!")
-    fmt.Fprintf(w, "%s\n", say("BLUE1031 Hello taoxb !!!"))
+    fmt.Fprintf(w, "%s\n", say("BLUE1123 Hello taoxb it's knative testing!!!"))
 }
 
 func main() {
