e5398af785d53715de25f7b58d92b46014c2620e
leotao <taoxb@163.com>
2020-06-21 12:11:04 +0800

    062101

--- src/picalc.go
+++ src/picalc.go
@@ -8,17 +8,17 @@ import (
 )
 
 func say(word string) string {
-    title := "[ 20200621 ] Hello world, this is"
+    title := "[ 2020062101 ] Hello world, this is"
     return title + " " + word
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
     log.Print("Say something, it's in container..20200621!")
-    fmt.Fprintf(w, "%s\n", say("GREEN20200621 Hello leo it's knative testing GREEN on github!!!"))
+    fmt.Fprintf(w, "%s\n", say("GREEN202006201 Hello leo it's knative testing GREEN on github!!!"))
 }
 
 func main() {
-    log.Print("Say something started.20200621 test hello leo")
+    log.Print("Say something started.2020062101 test hello leo")
 
     http.HandleFunc("/", handler)   
 
